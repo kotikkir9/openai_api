5e6b423f2a9a46889f519a97adecda14b607e2ac
kzh <kzh@freightsolution.io>
2023-03-11 03:16:40 +0100

    Added a check for token limit - if token amount is close to the maximum allowed value (4096), the program will be terminated

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import { createOrAppend, createFolder } from './utility.js';
 import { prompt, openai } from './setup.js';
 
 const CHAT_DIR = './chats';
+// in fact 4096, but there is no gpt-3.5-turbo tokenizer package for NodeJS yet, so I'm relying on the token usage provided by the server response.
+const TOKENS_LIMIT = 4000;
+
+let tokens = {};
 
 // creates the 'chats' dir if it doesn't exist
 createFolder(CHAT_DIR);
@@ -10,20 +14,18 @@ createFolder(CHAT_DIR);
 // creates a new file for each time the app in running
 const path = CHAT_DIR + '/' + new Date().toISOString().slice(0, 19).replace(/:/g, '-') + '.md';
 
-// main function
 (async () => {
     const dateString = new Date().toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-    let initialMessage = `Hello! I am ChatGPT, a large language model trained by OpenAI. My knowledge cutoff is set at 2021, and today\'s date is ${dateString}. How can I assist you today?\n`;
+    let initialMessage = `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Current date: ${dateString}`;
 
     const messages = [
-        // { role: 'assistant', content: initialMessage },
         { role: 'system', content: initialMessage },
     ];
 
     while (true) {
         try {
             const input = await prompt("> ");
-            input === 'exit' && process.exit(0);
+            (input.toLowerCase() === 'exit' || input.toLowerCase() === 'q') && process.exit(0);
 
             // Check whether content complies with OpenAI's usage policies.
             // https://platform.openai.com/docs/usage-policies/usage-policies
@@ -44,7 +46,6 @@ const path = CHAT_DIR + '/' + new Date().toISOString().slice(0, 19).replace(/:/g
                         break;
                     }
                 }
-
                 continue;
             }
 
@@ -54,12 +55,20 @@ const path = CHAT_DIR + '/' + new Date().toISOString().slice(0, 19).replace(/:/g
             const completion  = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
                 messages,
+                // max_tokens: 7,
+                // temperature: 1,
             });
 
+            tokens = completion.data.usage;
             const message = completion.data.choices[0].message;
-            console.log(chalk.cyan(message.content + '\n'));
             messages.push(message);
+            console.log(chalk.cyan(message.content + '\n'));
             createOrAppend(path, message.content, { response: true });
+
+            if (tokens?.total_tokens >= TOKENS_LIMIT) {
+                console.log(chalk.yellow(`Your token usage (${tokens?.total_tokens}) is close to the maximum allowed amount (4096).\nTherefore, this chat will be closed, see you next time.`))
+                process.exit(0);
+            }
 
         } catch (e) {
             console.error(chalk.red("Unable to prompt", e));
--- src/utility.js
+++ src/utility.js
@@ -6,17 +6,17 @@ export function createFolder(path) {
     }
 }
 
-export function createOrAppend(path, message = '', opt = {}) {
+export async function createOrAppend(path, message = '', opt = {}) {
     opt?.question && (message = '## ___' + message + '___');
     opt?.response && (message += '\n___\n<br />');
     message += '\n\n';
 
     if (!fs.existsSync(path)) {
-        fs.writeFile(path, message, function (err) {
+        fs.writeFileSync(path, message, function (err) {
             if (err) throw err;
         });
     } else {
-        fs.appendFile(path, message, (err) => {
+        fs.appendFileSync(path, message, (err) => {
             if (err) throw err;
         });
     }
